af91e37f1b2552eea17f2d9b7cf89ba057d820ac
Serban Adrian <adserban@deloittece.com>
2023-07-31 20:28:30 +0300

    deleted rows

--- src/test/java/Selenium/TestSelenium.java
+++ src/test/java/Selenium/TestSelenium.java
@@ -32,9 +32,7 @@ public class TestSelenium {
      private OrangeHRM orangeHRM;
      private CalculatorPage calculatorPage;
      private BrowserStack browserStack;
-     private GooglePage googlePage;
-     private GmailPage gmailPage;
-     private InternetHeroKuappPage internetHeroKuppaPage;
+
 
 
     @BeforeAll
@@ -43,9 +41,7 @@ public class TestSelenium {
          orangeHRM = initElements(driver,OrangeHRM.class);
          calculatorPage = initElements(driver, CalculatorPage.class);
          browserStack = initElements(driver,BrowserStack.class);
-         googlePage = initElements(driver, GooglePage.class);
-         gmailPage = initElements(driver, GmailPage.class);
-         internetHeroKuppaPage = initElements(driver, InternetHeroKuappPage.class);
+
      }
 
      @AfterAll
