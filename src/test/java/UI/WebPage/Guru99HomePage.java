9465971766e561503b34fd69b0f38b96aa511290
Serban Adrian <adserban@deloittece.com>
2023-08-03 14:55:23 +0300

    guru pages

--- src/test/java/UI/WebPage/Guru99HomePage.java
+++ src/test/java/UI/WebPage/Guru99HomePage.java
@@ -1,20 +1,20 @@
 package UI.WebPage;
 
+import UI.BasePage;
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
 
-public class Guru99HomePage {
+public class Guru99HomePage extends BasePage {
 
     WebDriver driver;
 
     By homePageUserName = By.xpath("//table//tr[@class='heading3']");
 
-    //Get the User name from Home Page
 
     public String getHomePageDashboardUserName(){
 
-        return    driver.findElement(homePageUserName).getText();
+        return findElementByCssSelector(homePageUserName).getText().toLowerCase();
 
     }
 
--- src/test/java/UI/WebPage/Guru99Login.java
+++ src/test/java/UI/WebPage/Guru99Login.java
@@ -1,21 +1,24 @@
 package UI.WebPage;
 
 import UI.BasePage;
+import com.beust.ah.A;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Guru99Login extends BasePage {
 
     WebDriver driver;
 
-    By user99GuruName = By.name("uid");
 
-    By password99Guru = By.name("password");
+    public final By USER = By.cssSelector("[name= 'uid']");
+    public final By PASS = By.cssSelector("[name= 'password']");
+    public final By TITTLE = By.cssSelector("[class= 'barone']");
+    public final By LOGIN = By.cssSelector("[name= 'btnLogin']");
 
-    By titleText =By.className("barone");
 
-    By login = By.name("btnLogin");
 
 
     public void goToGuruPage() {
@@ -27,28 +30,28 @@ public class Guru99Login extends BasePage {
 
     public void setUserName(String strUserName){
 
-        driver.findElement(user99GuruName).sendKeys(strUserName);
+        findElementByCssSelector(USER).sendKeys(strUserName);
 
     }
     //Set password in password textbox
 
     public void setPassword(String strPassword){
 
-        driver.findElement(password99Guru).sendKeys(strPassword);
+        findElementByCssSelector(PASS).sendKeys(strPassword);
     }
 
     //Click on login button
 
     public void clickLogin(){
 
-        driver.findElement(login).click();
+        findElementByCssSelector(LOGIN).click();
     }
 
     //Get the title of Login Page
 
     public String getLoginTitle(){
 
-        return driver.findElement(titleText).getText();
+        return findElementByCssSelector(TITTLE).getText().toLowerCase();
     }
 
     /**
@@ -63,7 +66,7 @@ public class Guru99Login extends BasePage {
 
      */
 
-    public void loginToGuru99(String strUserName,String strPasword){
+    public void loginToGuru99(String strUserName,String strPasword) {
 
         //Fill user name
         this.setUserName(strUserName);
@@ -75,4 +78,8 @@ public class Guru99Login extends BasePage {
         this.clickLogin();
     }
 
+    public void alert() {
+        switchToAlert();
+        acceptAlert();
+    }
 }
