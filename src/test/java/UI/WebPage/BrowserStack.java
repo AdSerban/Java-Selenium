8b2b2bfb3da155ab6bb0f7314d056a91eca1ee7f
Serban Adrian <adserban@deloittece.com>
2023-08-25 16:15:16 +0300

    updade

--- src/test/java/UI/WebPage/BrowserStack.java
+++ src/test/java/UI/WebPage/BrowserStack.java
@@ -10,5 +10,6 @@ public class BrowserStack {
     @FindBy(css = "[id='user_email_login']") public WebElement email;
     @FindBy(css = "[id='user_submit']") public WebElement subBtn;
     @FindBy(id = "tnc_checkbox") public WebElement termsAcc;
+    @FindBy(id = "tnc_checkbox") public WebElement termene;
 
 }
